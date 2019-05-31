303328d19b64c717be569f4f913f6c910671195e
Willard Cron <willardcron@gmail.com>
2019-05-31 09:31:43 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var options =["rock", "paper", "scissor"];
+
+// var userChoice; 
+// var choices = document.getElementsByClassName("choice");
+// console.log(choices);
+// for(var i = 0; i < choices.length; i++) {
+//   choices[i].addEventListener("click", function() {
+//     userChoice = this.id;
+//   compChoice = options[Math.floor(Math.random() * options.length)];
+//   document.getElementById("user_choice").innerHTML = userChoice
+//   document.getElementById("comp_choice").innerHTML = compChoice
+    // get the comp choices
+    // display choices
+    //figure out winner
+//   });
+// }
